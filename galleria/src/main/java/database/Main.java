24c18ae725498ab9966c5864889ce2e264b64f12
Armando <armando.laplaca@gmail.com>
2019-02-08 21:52:54 +0100

    commit iniziale

---
+++ galleria/src/main/java/database/Main.java
@@ -0,0 +1,24 @@
+package database;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//EntityFactor per la gestione delle instanze con il database
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("database-galleria");
+		EntityManager em = emf.createEntityManager();
+		
+		/*Utente u = new Utente(null,"Giorno","Giovanna");
+		em.getTransaction().begin();
+		em.persist(u);
+		
+		em.getTransaction().commit();*/
+		em.close();
+		emf.close();
+		System.out.println("NANI?");
+	}
+
+}
---
+++ galleria/src/main/java/database/Utente.java
@@ -0,0 +1,79 @@
+package database;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+public class Utente {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	@Column(length=30)
+	private String nome;
+	@Column(length=30)
+	private String password;
+	@Column(length=3)
+	private Integer permessi=1;
+	
+	public Utente(Integer id, String nome, String password, Integer permessi) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.password = password;
+		this.permessi = permessi;
+	}
+
+	public Utente(Integer id, String nome, String password) {
+		this.id = id;
+		this.nome = nome;
+		this.password = password;
+	}
+
+	public Utente(String nome, String password) {
+		this.nome = nome;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getPermessi() {
+		return permessi;
+	}
+
+	public void setPermessi(int permessi) {
+		this.permessi = permessi;
+	}
+
+	@Override
+	public String toString() {
+		return "Utente [id=" + id + ", nome=" + nome + ", password=" + password + ", permessi=" + permessi + "]";
+	}
+
+}
