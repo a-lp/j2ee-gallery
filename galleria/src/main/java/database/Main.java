c7de3e4b7bc2231aa1529c80161d17a483d05cac
armi9 <armi9@DESKTOP-HA1VT8O.homenet.telecomitalia.it>
2019-02-10 22:38:08 +0100

    aggiunti tag

--- galleria/src/main/java/database/Main.java
+++ galleria/src/main/java/database/Main.java
@@ -1,5 +1,13 @@
 package database;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,7 +18,48 @@ public class Main {
 	private static EntityManager em = emf.createEntityManager();
 	
 	public static void tester() {
-		
+		List<String> nomi = new ArrayList<String>();
+		List<String> cognomi = new ArrayList<String>();
+		List<String> foto = new ArrayList<String>();
+		Scanner s;
+		try {
+			s = new Scanner(new File("src/main/resources/nomi.txt"));
+			while (s.hasNext()){
+				nomi.add(s.next());
+			}
+			s.close();
+			s = new Scanner(new File("src/main/resources/cognomi.txt"));
+			while (s.hasNext()){
+				cognomi.add(s.next());
+			}
+			s.close();
+			s = new Scanner(new File("src/main/resources/foto.txt"));
+			while (s.hasNextLine()){
+				foto.add(s.nextLine());
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Collections.shuffle(nomi);
+		Collections.shuffle(cognomi);
+		Collections.shuffle(foto);
+		/*for(int i=0; i<nomi.size(); i++) {
+			String nome = nomi.get(i).toLowerCase()+"."+cognomi.get(i%cognomi.size()).toLowerCase()+"@email.com";
+			String password = nomi.get(i).toLowerCase()+cognomi.get(i%cognomi.size())+nomi.size()+cognomi.size();
+			add(new Utente(nome,password,null));
+		}*/
+		for(int i=0; i<foto.size(); i++) {		
+			String[] wow = (foto.get(i).toString()).split("&");
+			String url = wow[0];
+			String descrizione = wow[1];
+			int endIndex=((255<descrizione.length())?255:descrizione.length());
+			Double dimensione = (new Random()).nextDouble()*500;
+			Short altezza = (short) (new Random()).nextInt(Short.MAX_VALUE + 1);
+			Short larghezza = (short) (new Random()).nextInt(Short.MAX_VALUE + 1);
+			add(new Fotografia(url,dimensione,altezza,larghezza,null,descrizione.substring(0, endIndex)));
+		}
 	}
 
 	public static void add(Object u) {
@@ -41,7 +90,8 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		Utente u = new Utente("Giorno", "Giovanna", null);
+		tester();
+		/*Utente u = new Utente("Giorno", "Giovanna", null);
 		System.out.println("Aggiungo " + u);
 		add(u);
 		u.setPermessi((short)99);
@@ -51,7 +101,7 @@ public class Main {
 		}
 		else {
 			System.out.println("Aggiornamento con successo");
-		}
+		}*/
 		em.close();
 		emf.close();
 	}
--- galleria/src/main/java/utility/Scraper.java
+++ galleria/src/main/java/utility/Scraper.java
@@ -3,6 +3,8 @@ package utility;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,19 +18,35 @@ public class Scraper {
 		Document cognomi_html;
 		Elements tmp;
 		try {
-			// FOTO
+			// FOTO 
+			//TODO scrapare i tag da qua https://www.photoawards.com/categories/  
 			File input = new File("D:\\Documenti\\Progetto MAP\\file.html");
 			FileWriter foto_file = new FileWriter(
-					new File("C:\\Users\\armi9\\git\\map-unipa\\galleria\\src\\main\\resources\\foto.txt"));
+					new File("src/main/resources/foto.txt"));
 			foto_html = Jsoup.parse(input, "UTF-8", "https://images.nasa.gov/");
 			Elements links = foto_html.getElementsByTag("img");
 			for (Element link : links) {
 				if (!"".equals(link.attr("ng-src")))
-					foto_file.write(link.attr("ng-src") + "|||" + link.attr("alt") + "\n");
+					foto_file.write(link.attr("ng-src") + " ||| " + link.attr("alt") + "\n");
 			}
 			foto_file.close();
+			/*FileWriter tag_file = new FileWriter(
+					new File("src/main/resources/foto.txt"));
+			foto_html = Jsoup.connect("https://www.photoawards.com/categories/").get();
+			Elements links = foto_html.select("body > div.wrapper > div > div > div > div.full_width > div > div.vc_row.wpb_row.section.vc_row-fluid.white-page.grid_section > div > div > div > div > div > div:nth-child(2) > div.wpb_column.vc_column_container.vc_col-sm-8 > div > div > div.wpb_text_column.wpb_content_element > div > ul");
+			Set<String> tags = new HashSet<String>();
+			for (Element link : links) {
+				for(Element yolo: link.getElementsByTag("li")) {
+					tags.add(yolo.child(0).text());
+				}
+			}
+			for(String t: tags) {
+				tag_file.write(t+"\n");
+			}
+			tag_file.close();*/
+			
 			// NOMI
-			FileWriter nomi_file = new FileWriter(
+			/*FileWriter nomi_file = new FileWriter(
 					new File("C:\\Users\\armi9\\git\\map-unipa\\galleria\\src\\main\\resources\\nomi.txt"));
 			nomi_html = Jsoup.connect(
 					"https://www.behindthename.com/names/browse.php?type_gender=1&operator_gender=is&value_gender%5B%5D=unisex&type_popularity=1&operator_popularity=is&value_popularity%5B%5D=&yearoperator_popularity=is&year_popularity%5B%5D=&rankoperator_popularity=is&rank_popularity%5B%5D=")
@@ -46,7 +64,7 @@ public class Scraper {
 			for (Element a : tmp) {
 				cognomi_file.write(a.childNode(0).toString() + "\n");
 			}
-			cognomi_file.close();
+			cognomi_file.close();*/
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
